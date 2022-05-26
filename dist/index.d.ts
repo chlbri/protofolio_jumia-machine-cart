dbacc0f97a5d54a87f331e1dde3e49281cadbd9b
chlbri <51156116+chlbri@users.noreply.github.com>
2022-05-26 00:17:19 +0000

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const machine: import("xstate").StateMachine<unknown, any, import("xstate").AnyEventObject, {
-    value: any;
-    context: unknown;
-}, import("xstate").BaseActionObject, import("xstate").ServiceMap, import("xstate").ResolveTypegenMeta<import("xstate").TypegenDisabled, import("xstate").AnyEventObject, import("xstate").BaseActionObject, import("xstate").ServiceMap>>;
