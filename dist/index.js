69b44478c5d3463595320406181d0fbd1e5e2705
chlbri <bri_lvi@icloud.com>
2022-05-26 00:14:56 +0000

    ok

---
+++ dist/index.js
@@ -0,0 +1,198 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.machine = void 0;
+const xstate_1 = require("xstate");
+exports.machine = (0, xstate_1.createMachine)({
+    initial: 'idle',
+    states: {
+        idle: {
+            on: {
+                START: {
+                    target: 'checkingEnvironmentVariables',
+                },
+            },
+        },
+        checkingEnvironmentVariables: {
+            id: 'checkingEnvironmentVariables',
+            after: {
+                '10': {
+                    cond: 'allEnvironmentVariablesAreSet',
+                    target: 'starting',
+                },
+            },
+        },
+        starting: {
+            after: {
+                '10': {
+                    target: 'started',
+                },
+            },
+        },
+        started: {
+            initial: 'provisionning',
+            states: {
+                delivery: {
+                    id: 'delivery',
+                    initial: 'idle',
+                    states: {
+                        shippingEstimation: {
+                            invoke: {
+                                src: 'shipEstimation',
+                                id: 'shipEstimation',
+                                onDone: [
+                                    {
+                                        actions: 'assignShipCost',
+                                        target: 'readyForNext',
+                                    },
+                                ],
+                            },
+                        },
+                        readyForNext: {
+                            on: {
+                                NEXT: {
+                                    target: '#cart.started.payment',
+                                },
+                            },
+                        },
+                        validatingAddress: {
+                            invoke: {
+                                src: 'validateShippingAddress',
+                                onDone: [
+                                    {
+                                        target: 'shippingEstimation',
+                                    },
+                                ],
+                                onError: [
+                                    {
+                                        actions: 'assignErrorNotValidAddress',
+                                        target: '#cart.started.provisionning.provisionned',
+                                    },
+                                ],
+                            },
+                        },
+                        idle: {
+                            on: {
+                                ASSIGN_ADRESS: {
+                                    target: 'validatingAddress',
+                                },
+                            },
+                        },
+                    },
+                },
+                payment: {
+                    entry: 'startPaymentMachine',
+                    invoke: {
+                        src: 'payment',
+                        id: 'payment',
+                        onDone: 'confirmation',
+                    },
+                    initial: 'busy',
+                    states: {
+                        busy: {
+                            after: {
+                                200: 'notBusy',
+                            },
+                        },
+                        notBusy: {},
+                        processingVouchers: {
+                            after: {
+                                100: 'notBusy',
+                            },
+                        },
+                    },
+                    on: {
+                        'PAYMENT.NEXT': {
+                            actions: 'sendNextToPaymentMachine',
+                        },
+                        'PAYMENT.PAY.RECEPTION': {
+                            actions: 'sendVouchersToPaymentMachine',
+                            target: '.busy',
+                        },
+                        'PAYMENT.PAY.PAYPAL': {
+                            actions: 'sendPaypalToPaymentMachine',
+                            target: '.busy',
+                        },
+                        'PAYMENT.PAY.VISA': {
+                            actions: 'sendVisaToPaymentMachine',
+                            target: '.busy',
+                        },
+                        'PAYMENT.PAY.MOMO': {
+                            actions: 'sendMomoToPaymentMachine',
+                        },
+                        'PAYMENT.PAY.OM': {
+                            actions: 'sendOMToPaymentMachine',
+                        },
+                        'PAYMENT.PAY.OTHER': {
+                            actions: 'sendOtherToPaymentMachine',
+                        },
+                        'PAYMENT.PAY.VOUCHER': {
+                            actions: 'sendVoucherPayToPaymentMachine',
+                        },
+                        ABORT: {
+                            target: '#delivery',
+                            actions: 'abortPayment',
+                        },
+                    },
+                },
+                provisionning: {
+                    initial: 'notProvisionned',
+                    states: {
+                        notProvisionned: {
+                            on: {
+                                ADD_ARTICLES: {
+                                    actions: 'addArticles',
+                                    target: 'provisionning',
+                                },
+                            },
+                        },
+                        provisionning: {
+                            after: {
+                                '20': {
+                                    target: 'provisionned',
+                                },
+                            },
+                        },
+                        provisionned: {
+                            on: {
+                                NEXT: {
+                                    cond: 'cardIsNotEmpty',
+                                    target: '#cart.started.delivery',
+                                },
+                                ADD_ARTICLES: {
+                                    actions: 'addArticles',
+                                    target: 'provisionning',
+                                },
+                            },
+                        },
+                    },
+                },
+                confirmation: {
+                    id: 'confirmation',
+                    initial: 'articles',
+                    states: {
+                        articles: {
+                            entry: 'sendArticlesToShipping',
+                            exit: 'inc',
+                            always: {
+                                target: 'command',
+                            },
+                        },
+                        command: {
+                            entry: 'registerCommand',
+                            exit: 'inc',
+                            always: {
+                                target: '#final',
+                            },
+                        },
+                    },
+                },
+                final: {
+                    id: 'final',
+                    data: () => ({}),
+                    type: 'final',
+                },
+            },
+        },
+    },
+    id: 'cart',
+});
---
+++ dist/payment.d.ts
@@ -0,0 +1,4 @@
+export declare const payment: import("xstate").StateMachine<unknown, any, import("xstate").AnyEventObject, {
+    value: any;
+    context: unknown;
+}, import("xstate").BaseActionObject, import("xstate").ServiceMap, import("xstate").ResolveTypegenMeta<import("xstate").TypegenDisabled, import("xstate").AnyEventObject, import("xstate").BaseActionObject, import("xstate").ServiceMap>>;
